7cb913a0a3eade2c49073e30e21dbc83a5a9b930
AJIT YADAV <89628136+Ajit20000@users.noreply.github.com>
2022-02-04 12:54:56 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,92 +1,47 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 31 16:05:41 2022
-
-@author: Sujit
-"""
-
+#import package
 import streamlit as st
 import pandas as pd
-import shap
-import matplotlib.pyplot as plt
-from sklearn import datasets
-from sklearn.ensemble import RandomForestRegressor
-st.set_option('deprecation.showPyplotGlobalUse', False)
-st.write("""
-# Boston House Price Prediction App
-This app predicts the **Boston House Price**!
-""")
-st.write('---')
+import numpy as np
+#from PIL import Image
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+#import time
 
-# Loads the Boston House Price Dataset
-boston = datasets.load_boston()
-X = pd.DataFrame(boston.data, columns=boston.feature_names)
-Y = pd.DataFrame(boston.target, columns=["MEDV"])
+#import the data
+data = pd.read_csv("Data Clean.csv")
+#image = Image.open("house.png")
+st.title("Welcome to the House Price Prediction App")
+#st.image(image, use_column_width=True)
 
-# Sidebar
-# Header of Specify Input Parameters
-st.sidebar.header('Specify Input Parameters')
+#checking the data
+st.write("This is an application for knowing how much range of house prices you choose using machine learning. Let's try and see!")
+check_data = st.checkbox("See the simple data")
+if check_data:
+    st.write(data.head())
+st.write("Now let's find out how much the prices when we choosing some parameters.")
 
-def user_input_features():
-    CRIM = st.sidebar.slider('CRIM', X.CRIM.min(), X.CRIM.max(), X.CRIM.mean())
-    ZN = st.sidebar.slider('ZN', X.ZN.min(), X.ZN.max(), X.ZN.mean())
-    INDUS = st.sidebar.slider('INDUS', X.INDUS.min(), X.INDUS.max(), X.INDUS.mean())
-    CHAS = st.sidebar.slider('CHAS', X.CHAS.min(), X.CHAS.max(), X.CHAS.mean())
-    NOX = st.sidebar.slider('NOX', X.NOX.min(), X.NOX.max(), X.NOX.mean())
-    RM = st.sidebar.slider('RM', X.RM.min(), X.RM.max(), X.RM.mean())
-    AGE = st.sidebar.slider('AGE', X.AGE.min(), X.AGE.max(), X.AGE.mean())
-    DIS = st.sidebar.slider('DIS', X.DIS.min(), X.DIS.max(), X.DIS.mean())
-    RAD = st.sidebar.slider('RAD', X.RAD.min(), X.RAD.max(), X.RAD.mean())
-    TAX = st.sidebar.slider('TAX', X.TAX.min(), X.TAX.max(), X.TAX.mean())
-    PTRATIO = st.sidebar.slider('PTRATIO', X.PTRATIO.min(), X.PTRATIO.max(), X.PTRATIO.mean())
-    B = st.sidebar.slider('B', X.B.min(), X.B.max(), X.B.mean())
-    LSTAT = st.sidebar.slider('LSTAT', X.LSTAT.min(), X.LSTAT.max(), X.LSTAT.mean())
-    data = {'CRIM': CRIM,
-            'ZN': ZN,
-            'INDUS': INDUS,
-            'CHAS': CHAS,
-            'NOX': NOX,
-            'RM': RM,
-            'AGE': AGE,
-            'DIS': DIS,
-            'RAD': RAD,
-            'TAX': TAX,
-            'PTRATIO': PTRATIO,
-            'B': B,
-            'LSTAT': LSTAT}
-    features = float(pd.DataFrame(data, index=[0]))
-    return features
+#input the numbers
+sqft_liv = st.slider("What is your square feet of living room?",int(data.sqft_living.min()),int(data.sqft_living.max()),int(data.sqft_living.mean()) )
+bath     = st.slider("How many bathrooms?",int(data.bathrooms.min()),int(data.bathrooms.max()),int(data.bathrooms.mean()) )
+bed      = st.slider("How many bedrooms?",int(data.bedrooms.min()),int(data.bedrooms.max()),int(data.bedrooms.mean()) )
+floor    = st.slider("How many floor do you want?",int(data.floors.min()),int(data.floors.max()),int(data.floors.mean()) )
 
-df = float(user_input_features())
+#splitting your data
+X = data.drop('price', axis = 1)
+y = data['price']
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=.2, random_state=45)
 
-# Main Panel
+#modelling step
+#import your model
+model=LinearRegression()
+#fitting and predict your model
+model.fit(X_train, y_train)
+model.predict(X_test)
+errors = np.sqrt(mean_squared_error(y_test,model.predict(X_test)))
+predictions = model.predict([[sqft_liv,bath,bed,floor]])[0]
 
-# Print specified input parameters
-st.header('Specified Input parameters')
-st.write(df)
-st.write('---')
-
-# Build Regression Model
-model = RandomForestRegressor()
-model.fit(X, Y)
-# Apply Model to Make Prediction
-prediction = model.predict(df)
-
-st.header('Prediction of MEDV')
-st.write(prediction)
-st.write('---')
-
-# Explaining the model's predictions using SHAP values
-# https://github.com/slundberg/shap
-explainer = shap.TreeExplainer(model)
-shap_values = explainer.shap_values(X)
-
-st.header('Feature Importance')
-plt.title('Feature importance based on SHAP values')
-shap.summary_plot(shap_values, X)
-st.pyplot(bbox_inches='tight')
-st.write('---')
-
-plt.title('Feature importance based on SHAP values (Bar)')
-shap.summary_plot(shap_values, X, plot_type="bar")
-st.pyplot(bbox_inches='tight')
+#checking prediction house price
+if st.button("Run me!"):
+    st.header("Your house prices prediction is USD {}".format(int(predictions)))
+    st.subheader("Your range of prediction is USD {} - USD {}".format(int(predictions-errors),int(predictions+errors) ))
